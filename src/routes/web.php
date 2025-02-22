4d1ff847da495ea5cc5d078746412b1bee2c2dc0
s-kim-sysnavi <s-kim@sysnavi.co.jp>
2025-02-22 16:58:59 +0900

    update:qrcode_conversion

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\QrcodeConversionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/input', [QrcodeConversionController::class, 'input'])->name('input');
+Route::get('/show', [QrcodeConversionController::class, 'show'])->name('show');
